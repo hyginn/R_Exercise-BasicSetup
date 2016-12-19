5a1d8961bf1c45173d5538cd25cefeaf4f51cee5
hyginn <boris.steipe@utoronto.ca>
2016-12-19 07:56:26 +0100

    Finalize text

--- R_Exercise-BasicSetup.R
+++ R_Exercise-BasicSetup.R
@@ -25,19 +25,30 @@
 # Now go through the tasks below, then return to the Wiki tutorial. But keep
 # RStudio open so you can continue to use it in the tutorial.
 
-# ==== The RStudio layout
-#  In the default layout of RStudio's panes, this script file has been opened in the top-left pane; I refer to this as the "Script Pane". This is where you read and edit files.
+# ==== The RStudio layout ======================================================
 
-#  The pane below (bottom-left) is the "Console". This is where you type commands, or execute them from a script. This is also where RStudio produces its output.
+# In the default layout of RStudio's panes, this script file has been opened in
+# the top-left pane; I refer to this as the "Script Pane". This is where you
+# read and edit files.
 
-# The pane at the top-right is the "Environment Pane". This is where you can see which variables or functions have currently been defined ("assigned") and what their values are.
+# The pane below (bottom-left) is the "Console". This is where you type
+# commands, or execute them from a script. This is also where RStudio produces
+# its output.
 
-# The pane at the bottom-right displays different kinds of information in its tabs: In the "Files Tab" you see a directory listing of the contents of the current working directory. The "Plots Tab" shows plots and graphs that R has created. And the "Help Tab" displays the information in R's help system. (We probably won't be using the other tabs.)
+# The pane at the top-right is the "Environment Pane". This is where you can see
+# which variables or functions have currently been defined ("assigned") and what
+# their values are.
+
+# The pane at the bottom-right displays different kinds of information in its
+# tabs: In the "Files Tab" you see a directory listing of the contents of the
+# current working directory. The "Plots Tab" shows plots and graphs that R has
+# created. And the "Help Tab" displays the information in R's help system. (We
+# probably won't be using the other tabs.)
 
 
-# ==== Execute some code
+# ==== Execute some code =======================================================
 #  You can execute code by typing it into the console.
-#  Type: 3 + 4
+#  Type: 3 + 4  and then hit <enter>
 #
 #  Now type: 1 + sqrt(5) / 2
 #  (This is NOT the Golden Ratio, phi)
@@ -53,16 +64,17 @@
 #
 
 # You can ALSO execute code by selecting and pressing <cmd><enter>. This passes
-# code from the script window to the console and executes it automatically. This
+# code from the Script Pane to the console and executes it automatically. This
 # is very convenient - in fact, this is the preferred way to work with lengthy
 # scripts. There are several variants:
 
-#  - When nothing is selected, pressing <cmd><enter> will execute the current
-#    line and move the cursor to the next line. You can walk through code line
-#    by line in this way.
+#  - When nothing is selected in the Script Pane, pressing <cmd><enter> will
+#    execute the current line and move the cursor to the next line. You can
+#    "walk" through code line by line in this way.
 #  - This is the same as if you would have selected an entire line.
+
 #  - If you select more than one line, you can execute an entire block of code
-#    at once. Try this: select the block of code below (lines 69 to 77),
+#    at once. Try this: select the block of code below (lines 82 to 90),
 #    and hit <cmd><enter> to execute it. The code calculates successive
 #    approximations of the Golden Ratio from Fibonacci numbers, and then prints
 #    the "true" value.
@@ -77,50 +89,62 @@ for (i in 1:20) {
 }
 print((1 + sqrt(5)) / 2, digits = 10)
 
-# - but it's also really useful to be able to select _less_ than one line of
+# - But it's also really useful to be able to select _less_ than one line of
 #   code. For example, this allows us to analyze complex, nested R expressions
 #   from the inside out.
 
 
-# ==== Change some code and save the change
+# ==== Change some code and save the change ====================================
 
 # Here is a bit of code that plots 1.5 turns of the Golden Spiral. Don't worry
 # if you don't understand the details - that's not the point right now. Execute
 # the code.
 
-turns <- 1.5     # number of turns of the spiral to plot
-p <- seq(0, (turns * 2 * pi), length.out = round(turns * 100)) # Intervals
-plot(exp(0.3061868562 * p) * cos(p),      # x-coordinates
-     exp(0.3061868562 * p) * sin(p),      # y-coordinates
-     type = "l", col = "#CC0000",         # draw as a red line
-     xaxt = "n", yaxt = "n",              # turn tick-marks off
-     xlab = "", ylab = "",                # turn axis labels off
-     asp = 1.0)                           # plot with 1:1 aspect ratio
-abline(v = 0, col = "#E6EEFF")            # add pale-blue vertical ..
-abline(h = 0, col = "#E6EEFF")            # ... and horizontal line
+turns <- 1.5                                 # number of turns to plot
+p <- seq(0, (turns * 2 * pi),
+         length.out = round(turns * 100))    # intervals
+plot(exp(0.3061868562 * p) * cos(p),         # x-coordinates
+     exp(0.3061868562 * p) * sin(p),         # y-coordinates
+     type = "l", col = "#CC0000",            # draw as a red line
+     xaxt = "n", yaxt = "n",                 # turn tick-marks off
+     xlab = "", ylab = "",                   # turn axis labels off
+     asp = 1.0)                              # plot with 1:1 aspect ratio
+abline(v = 0, col = "#E6EEFF")               # add pale-blue vertical ...
+abline(h = 0, col = "#E6EEFF")               # ... and horizontal line
 
 # Now change the value of "turns" from 1.5 to 2.75 in the code above. Note that
-# the filename in the script tab changes colour: it turns red and has a star
+# the filename in the Script Pane changes colour: it turns red and has a star
 # behind it. This means: this script has unsaved changes.
 
 # Execute the modified block of code. The number of the turns increases.
 
 # Now save the change. The filename changes to black again.
 
-# ==== Quit, and restart where you left off
+# ==== Quit, and restart where you left off ====================================
 
-# Finally, we'll quit RStudio and restart the project. It should be obvious how to quit. But here's how to reload a project.
+# Finally, we'll quit RStudio and restart the project. It should be obvious how
+# to quit. But here's how to reload a project.
 
 # Choose: File -> Recent Projects -> <the project you need>
-# Note: You want the "Recent Projects" menu option, not "Recent Files".
 
-# Try this, use the File -> Quit Session... option to quit - and if you are
+# Note: Take care to choose the "Recent Projects" menu option,
+#       not "Recent Files".
+
+# Try this out, use the File -> Quit Session... option to quit - and if you are
 # being asked if you want to save anything, for the purpose of this course or
 # workshop _always_ answer "No". Then reload the project, note that the value of
-# turns is now what you had changed it to.
+# turns should now be what you had changed it to.
 
-# Plot another beautiful spiral for good measure and then return to the tutorial
-# Wiki page.
+# Plot another beautiful spiral for good measure.
+
+# Review: - The layout of the Panes
+#         - Typing code
+#         - Editing code in the Console, using the arrow keys
+#         - Selecting code in the Script Pane and executing it with <cmd><enter>
+#         - Editing code in the script and saving the change
+#         - Quitting RStudio and restarting a recent project.
+
+# Finally, return to the tutorial Wiki page.
 
 
 # [END]
