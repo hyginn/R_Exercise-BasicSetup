78e9325104efba6ca5d3c4eabac6ac285fac2a15
hyginn <boris.steipe@utoronto.ca>
2016-12-21 03:11:46 +0800

    Add comments, source .utilities.R

--- .init.R
+++ .init.R
@@ -1,8 +1,17 @@
 # .init.R
 # Functions to initialize this Exercise session
+#
+# Purpose: - Run the utilities.R script to load some functions and data into
+#            the Workspace.
+#          - Open the R_Exercise-BasicSetup.R script in the Script Pane.
+#
 # Boris Steipe
-# ====================================================================
+# ==============================================================================
+
+source(".utilities.R")
 
 file.edit("R_Exercise-BasicSetup.R")
+
+
 
 # [End]
